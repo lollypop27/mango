07448bdd238504d89dfd101b1964b19a8f844c17
lollypop27 <lara@adafctory.co.za>
2017-09-13 07:53:40 +0200

    adds producst with price & passing test

--- lib/mango_web/catalog/catalog.ex
+++ lib/mango_web/catalog/catalog.ex
@@ -1,5 +1,14 @@
 defmodule Mango.Catalog do
+  alias Mango.Catalog.Product
+
   def list_products do
-    ["Tomato", "Apple"]
+    product1 = %Product{ name: "Tomato", price: 50, is_seasonal: false }
+    product2 = %Product{ name: "Apple", price: 100, is_seasonal: true }
+    [product1, product2]
+  end
+
+  def list_seasonal_products do
+    list_products()
+    |> Enum.filter(fn(product) -> product.is_seasonal == true end)
   end
 end
